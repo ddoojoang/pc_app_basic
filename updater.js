3bb9009c2827ad9743b0db4b78e335ada09ac218
ddoojoang <ddoojoang@gmail.com>
2024-06-11 16:35:35 +0900

    first initial commit

--- updater.js
+++ updater.js
@@ -16,24 +16,25 @@ autoUpdater.setFeedURL({
   owner: 'ddoojoang',
   repo: 'test',
   token: token,
-  private: true,
+  // private: true,
 });
 
 module.exports = () => {
   console.log('업데이터 함수 호출됨');
   autoUpdater.checkForUpdates();
 
-  autoUpdater.on('update-available', () => {
-    dialog.showMessageBox({
-      type: 'info',
-      title: '업데이트 가능',
-      message: '새로운 버전이 있습니다. 지금 업데이트하시겠습니까?',
-      buttons: ['업데이트', '나중에']
-    }).then(result => {
-      let buttonIndex = result.response;
-      if (buttonIndex === 0) autoUpdater.downloadUpdate();
-    });
-  });
+  // 기존 코드 주석 처리
+  // autoUpdater.on('update-available', () => {
+  //   dialog.showMessageBox({
+  //     type: 'info',
+  //     title: '업데이트 가능',
+  //     message: '새로운 버전이 있습니다. 지금 업데이트하시겠습니까?',
+  //     buttons: ['업데이트', '나중에']
+  //   }).then(result => {
+  //     let buttonIndex = result.response;
+  //     if (buttonIndex === 0) autoUpdater.downloadUpdate();
+  //   });
+  // });
 
   autoUpdater.on('update-downloaded', () => {
     dialog.showMessageBox({
@@ -46,4 +47,15 @@ module.exports = () => {
       if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
     });
   });
+
+  // 새로운 자동 업데이트 코드
+  autoUpdater.on('update-available', () => {
+    // 자동으로 업데이트를 다운로드
+    autoUpdater.downloadUpdate();
+  });
+
+  // autoUpdater.on('update-downloaded', () => {
+  //   // 다운로드 완료 후 자동으로 설치 및 재시작
+  //   autoUpdater.quitAndInstall(false, true);
+  // });
 };
